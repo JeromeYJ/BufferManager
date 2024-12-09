86bf82b299821b4db3d2909295e5245cfb82fc13
JeromeYJ <3064325896@qq.com>
2024-12-09 16:08:33 +0800

    添加主要目录结构与头文件

---
+++ include/Common.h
@@ -0,0 +1,27 @@
+
+
+using namespace std;
+
+#define DEFBUFSIZE 1024   // buffer大小
+#define FRAMESIZE 4096    // 页帧大小
+#define MAXPAGES 50000    // 磁盘最大页数
+
+
+// 表示页帧frame的结构体
+struct bFrame
+{
+    char field [FRAMESIZE ];
+};
+
+
+// Buffer Control Blocks
+struct BCB
+{
+    BCB();
+    int page_id;
+    int frame_id;
+    int latch;
+    int count;
+    int dirty;
+    BCB * next;
+};
